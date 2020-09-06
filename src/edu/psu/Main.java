0798418aa5c7cb8602bc48eda4c700ac7fb80918
cuprumtan <cuprumtan@gmail.com>
2020-09-06 23:05:06 +0500

    Добавлена генерация данных и создание ненормализованной таблицы

--- src/edu/psu/Main.java
+++ src/edu/psu/Main.java
@@ -1,8 +1,14 @@
 package edu.psu;
 
+import static edu.psu.SQLiteDB.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        String url = "jdbc:sqlite:perm_city_polyclinic_7_registry.db";
+        String tableName = "perm_city_polyclinic_7_registry";
+        createNewDatabase(url);
+        createNewTable(url, tableName);
+        insertIntoTable(url, tableName);
     }
 }
